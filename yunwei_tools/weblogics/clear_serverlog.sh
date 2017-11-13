04e421bf193b5e15653836624e1919cfdf505f55
harryyou <137248202@qq.com>
2017-11-13 16:52:27 +0800

    清理weblogic server日志

---
+++ yunwei_tools/weblogics/clear_serverlog.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#coding:utf-8
+
+. ${EPICC_FUNCTION}
+
+#给定domain存放的路径
+if [ -n "$DOMAINS_HOME" ]
+    then logging "Domain 路径为：" $DOMAINS_HOME
+else
+    DOMAINS_HOME=${HOME}/bea/user_projects/domains
+    logging "默认Domain 路径为：" $DOMAINS_HOME
+fi
+
+echo "清理${DOMAINS_HOME}/logs下的日志："
+for i in `find $DOMAINS_HOME/*/logs -name *.log -mtime +7`
+do
+    echo $i
+    rm -f $i
+done
+
+echo "清理${DOMAINS_HOME}/*domain/servers/*server/logs 下的日志："
+for i in `find ${DOMAINS_HOME}/*/servers/*/logs -name *.log -mtime +7`
+do
+    echo $i
+    rm -f $i
+done
