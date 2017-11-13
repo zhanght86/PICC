2b3503e29b813cb7dd6d06d2f21ab8d824615ec4
harryyou <137248202@qq.com>
2017-11-13 16:08:23 +0800

    运维工具重启weblogic服务功能

---
+++ yunwei_tools/weblogics/startstop.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#coding:utf-8
+
+. ${EPICC_FUNCTION}
+
+while true
+do
+    cat << EOF
+########################################################################################################################
+<1> 停止weblogic 服务
+<2> 重启weblogic 服务
+<0> 返回上一级菜单
+########################################################################################################################
+EOF
+    
+    read choice
+    if [ "$choice" = 0 ]
+        then break
+
+    elif [ "$choice" = "1" ]
+        then logging "没有写停止的脚本，手动杀吧！"
+        sleep 3
+
+    elif [ "$choice" = 2 ]
+        then
+        n=1
+        declare restart
+        restart[0]="<0> 返回上一级菜单"
+        #遍历restart—shell目录下的重启脚本
+        #for i in `find /home/weblogic/yunwei/restart_shell -name *restart.sh`;do restart[$n]=$i;n=`expr $n + 1`;done
+        for i in `find ${RESTART_DIR} -name *restart.sh`;do restart[$n]=$i; n=`expr $n + 1`;done
+        echo "########################################################################################################################"
+        for i in ${!restart[@]};do if [ $i = 0 ];then echo -e ${restart[$i]};else echo -e "<$i>" `basename ${restart[$i]}`;fi;done
+        echo "########################################################################################################################"
+        read choice
+        if [ "$choice" = "0" ]
+            then continue
+
+        elif [[ ${!restart[@]} =~ $choice ]]
+            then sh ${restart[$choice]}
+        else
+            logging "为啥不按提示的输呢？"
+        fi
+    else
+        continue
+    fi
+done
+
